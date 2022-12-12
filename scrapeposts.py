b0ead78d3d0c5a8cdd37016e4e023f4b6df9be6a
Jeremy Macks <31860185+macksjeremy@users.noreply.github.com>
2022-12-12 18:09:01 -0500

    Added Authors.txt

--- scrapeposts.py
+++ scrapeposts.py
@@ -1,6 +1,6 @@
 import praw
 from private.details import init
-
+import os
 def chomp(s):
     s = s.replace('\n','')
     return s
@@ -11,4 +11,22 @@ for i in range(len(authors)):
     authors[i] = chomp(authors[i])
 
 print(authors)
+cwd = os.getcwd()
+datapath = os.path.join(cwd,"data")
+if not os.path.exists(datapath):
+    os.mkdir(datapath)
 reddit = init()
+for i in authors:
+    print("Finding Comments for", i)
+    allcontent = ""
+    newpath = os.path.join(datapath,i)
+    if not os.path.exists(newpath):
+        os.mkdir(newpath)
+    allcomments = reddit.redditor(i).comments.hot(limit=None)
+    for comment in allcomments:
+        allcontent += "[SEP]"
+        allcontent += comment.body
+    savepath = os.path.join(newpath, "contents.txt")
+    with open(savepath,'w',encoding="utf-8") as f:
+        f.write(allcontent)
+
