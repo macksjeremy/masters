738f11b3594407edec409ba9a98d9dc23f4ae029
Jeremy Macks <31860185+macksjeremy@users.noreply.github.com>
2023-02-14 14:50:40 -0500

    Added Verbosity

--- getstats.py
+++ getstats.py
@@ -1,14 +1,24 @@
+import numpy as np
 import textstat
-def obtainstatistic(test_data):
-    print("Calculating Statistics for:", test_data)
-    print("-------------------------------------------------------------------------------------")
-    print("fleschreadingease:", textstat.flesch_reading_ease(test_data))
-    print("kincaid grade",textstat.flesch_kincaid_grade(test_data))
-    print("smog index", textstat.smog_index(test_data))
-    print("coleman index", textstat.coleman_liau_index(test_data))
-    print("automated readability index",textstat.automated_readability_index(test_data))
-    print("dale_chall readability:", textstat.dale_chall_readability_score(test_data))
-    print("Difficult words", textstat.difficult_words(test_data))
-    print("linsear:",textstat.linsear_write_formula(test_data))
-    print("-------------------------------------------------------------------------------------")
-    return
+def obtainstatistic(test_data, verbose=False):
+    flesch = textstat.flesch_reading_ease(test_data)
+    kincaid = textstat.flesch_kincaid_grade(test_data)
+    smog = textstat.smog_index(test_data)
+    liau = textstat.coleman_liau_index(test_data)
+    ari = textstat.automated_readability_index(test_data)
+    dale = textstat.dale_chall_readability_score(test_data)
+    difficult = textstat.difficult_words(test_data)
+    linsear = textstat.linsear_write_formula(test_data)
+    if(verbose):
+        print("Calculating Statistics for:", test_data)
+        print("-------------------------------------------------------------------------------------")
+        print("fleschreadingease:",flesch )
+        print("kincaid grade",kincaid)
+        print("smog index", smog)
+        print("coleman index",liau )
+        print("automated readability index",ari)
+        print("dale_chall readability:", dale)
+        print("Difficult words", difficult)
+        print("linsear:",linsear)
+        print("-------------------------------------------------------------------------------------")
+    return np.asarray([flesch,kincaid,smog,liau,ari,dale,difficult,linsear])
